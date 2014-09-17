38a3c231def5aa856ec14c45b4fe45f03d1b4842
Amit Aryeh Levy <amit@amitlevy.com>
2014-09-17 13:05:35 -0700

    more refactoring

--- router.go
+++ router.go
@@ -5,87 +5,101 @@ import (
   "sort"
 )
 
+func (this *Manager) RouteFindByTypeValue(req ManagerRequest) {
+  findReq, err := ParseFindByTypeValueRequest(req.msg)
+  if err != nil {
+    resp := NewError(ATT_OPCODE_FIND_BY_TYPE_VALUE_REQUEST, 0, 4)
+    req.device.Respond(resp.msg)
+    return
+  } else {
+    startHandle := findReq.StartHandle()
+    endHandle := findReq.EndHandle()
+    attType := findReq.Type()
+    attVal := findReq.Value()
+
+    handles := make(GroupValueLst, 0, 100)
+    for _,device := range this.devices {
+      if device == req.device || device.handleOffset < 0 {
+        continue
+      }
+      if device.handleOffset > int(endHandle) {
+        break
+      }
+
+      for _, handle := range device.handles {
+        if handle.uuid == attType && bytes.Equal(handle.cachedValue, attVal) {
+          offset := uint16(device.handleOffset)
+          h := &GroupValue{handle.handle + offset, handle.endGroup + offset, nil}
+          handles = append(handles, h)
+        }
+      }
+    }
+
+    sort.Sort(handles)
+
+    if len(handles) > 0 {
+      resp := NewFindByTypeValueResponse(handles)
+      req.device.Respond(resp.msg)
+    } else {
+      resp := NewError(ATT_OPCODE_FIND_BY_TYPE_VALUE_REQUEST, startHandle, 0x0A)
+      req.device.Respond(resp.msg)
+    }
+  }
+}
+
+func (this *Manager) RouteReadByType(req ManagerRequest) {
+  readReq, err := ParseReadByTypeRequest(req.msg)
+  if err != nil {
+    resp := NewError(ATT_OPCODE_READ_BY_TYPE_REQUEST, 0, 4)
+    req.device.Respond(resp.msg)
+    return
+  }
+
+  startHandle := readReq.StartHandle()
+  endHandle := readReq.EndHandle()
+  attType := readReq.Type()
+
+
+  for _,device := range this.devices {
+    offset := uint16(device.handleOffset)
+    if startHandle >= offset + 1 &&
+       startHandle <= offset + uint16(len(device.handles)) {
+      remoteReq := NewReadByTypeRequest(startHandle - offset,
+                    endHandle - offset, attType)
+      respBuf, err := device.Transaction(remoteReq.msg)
+      if err != nil {
+        resp := NewError(ATT_OPCODE_READ_BY_TYPE_REQUEST, 0, 4)
+        req.device.Respond(resp.msg)
+        return
+      }
+      if respBuf[0] == ATT_OPCODE_ERROR {
+        resp := NewError(ATT_OPCODE_READ_BY_TYPE_REQUEST, startHandle, 0x0A)
+        req.device.Respond(resp.msg)
+      } else {
+        segLen := int(respBuf[1])
+        for i := 2; i < len(respBuf); i += segLen {
+          h := uint16(respBuf[i]) + uint16(respBuf[i + 1]) << 8
+          h += offset
+          respBuf[i] = byte(h & 0xff)
+          respBuf[i + 1] = byte(h >> 8)
+        }
+        req.device.Respond(respBuf)
+      }
+      return
+    }
+  }
+  req.device.Respond(NewError(ATT_OPCODE_READ_BY_TYPE_REQUEST, 0, 0x0A).msg)
+}
+
 func (this *Manager) RunRouter() {
   for req := range this.requestChan {
     pkt := req.msg
     switch(pkt[0]) {
     case ATT_OPCODE_FIND_BY_TYPE_VALUE_REQUEST:
-      findReq, err := ParseFindByTypeValueRequest(pkt)
-      if err != nil {
-        resp := NewError(ATT_OPCODE_FIND_BY_TYPE_VALUE_REQUEST, 0, 4)
-        req.device.Respond(resp.msg)
-        continue
-      }
-
-      startHandle := findReq.StartHandle()
-      endHandle := findReq.EndHandle()
-      attType := findReq.Type()
-      attVal := findReq.Value()
-
-      handles := make(GroupValueLst, 0, 100)
-      for _,device := range this.devices {
-        if device == req.device || device.handleOffset < 0 {
-          continue
-        }
-        if device.handleOffset > int(endHandle) {
-          break
-        }
-
-        for _, handle := range device.handles {
-          if handle.uuid == attType && bytes.Equal(handle.cachedValue, attVal) {
-            offset := uint16(device.handleOffset)
-            h := &GroupValue{handle.handle + offset, handle.endGroup + offset, nil}
-            handles = append(handles, h)
-          }
-        }
-      }
-
-      sort.Sort(handles)
-
-      if len(handles) > 0 {
-        resp := NewFindByTypeValueResponse(handles)
-        req.device.Respond(resp.msg)
-      } else {
-        resp := NewError(ATT_OPCODE_FIND_BY_TYPE_VALUE_REQUEST, startHandle, 0x0A)
-        req.device.Respond(resp.msg)
-      }
+      this.RouteFindByTypeValue(req)
     case ATT_OPCODE_READ_BY_TYPE_REQUEST:
-      readReq, err := ParseReadByTypeRequest(pkt)
-      if err != nil {
-        resp := NewError(ATT_OPCODE_READ_BY_TYPE_REQUEST, 0, 4)
-        req.device.Respond(resp.msg)
-        continue
-      }
+      this.RouteReadByType(req)
 
-      startHandle := readReq.StartHandle()
-      endHandle := readReq.EndHandle()
-      attType := readReq.Type()
-
-
-      //handles := make(GroupValueLst, 0, 100)
-      for _,device := range this.devices {
-        offset := uint16(device.handleOffset)
-        if startHandle >= offset + 1 &&
-           startHandle <= offset + uint16(len(device.handles)) {
-          remoteReq := NewReadByTypeRequest(startHandle - offset,
-                        endHandle - offset, attType)
-          respBuf, _ := device.Transaction(remoteReq.msg)
-          if respBuf[0] == ATT_OPCODE_ERROR {
-            resp := NewError(ATT_OPCODE_READ_BY_TYPE_REQUEST, startHandle, 0x0A)
-            req.device.Respond(resp.msg)
-          } else {
-            segLen := int(respBuf[1])
-            for i := 2; i < len(respBuf); i += segLen {
-              h := uint16(respBuf[i]) + uint16(respBuf[i + 1]) << 8
-              h += offset
-              respBuf[i] = byte(h & 0xff)
-              respBuf[i + 1] = byte(h >> 8)
-            }
-            req.device.Respond(respBuf)
-          }
-          break
-        }
-      }
     case ATT_OPCODE_READ_REQUEST:
       fallthrough
     case ATT_OPCODE_READ_BLOB_REQUEST:
